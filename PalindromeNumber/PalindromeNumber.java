abeb644af3a439e57ddcc9f40a20d3824adc6609
Tobi <AdeniyiSoftware@gmail.com>
2020-08-26 11:28:38 +0100

    All previous solution in Java C++ and Python

---
+++ PalindromeNumber/PalindromeNumber.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if( x < 0){
+            return false;
+        }
+        int res = 0;
+        int t = x;
+        while(t != 0){
+            res = res * 10 + t % 10;
+            t /= 10;
+        }
+        return res == x;
+        
+    }
+}
